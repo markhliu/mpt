6de2fde1f277305ff1a7249bc4ccaef1f5b4f32f
markhliu <50116107+markhliu@users.noreply.github.com>
2020-08-21 09:51:28 -0400

    Add files via upload

---
+++ mptpkg/__init__.py
@@ -0,0 +1,14 @@
+from .mysr import voice_to_text
+from .mysay import print_say
+from .mywakeup import wakeup
+from .mytimer import timer
+from .myalarm import alarm
+from .myjoke import joke
+from .myemail import email
+from .myknowall import know_all
+from .mymusic import music_play, music_stop
+from .mynews import news_brief, news_stop
+from .myradio import live_radio, radio_stop
+from .myttt import ttt
+from .myconn import conn
+from .mystock import stock_market, stock_price
---
+++ mptpkg/myknowall.py
@@ -0,0 +1,25 @@
+import wolframalpha
+import wikipedia
+
+from mptpkg import print_say
+
+# Put your WolframApha APIkey below
+APIkey = {your APIkey} 
+wolf = wolframalpha.Client(APIkey)
+
+def know_all(v_inp):
+    # Use try and except to prevent crashing
+    try:
+        # Look for answer in Wolfram Alpha
+        res = wolf.query(v_inp)
+        # Create a string variable ans to contain answers
+        print_say(next(res.results).text)
+    except:
+        try:
+            ans=wikipedia.summary(v_inp)
+            print_say(ans[0:200])
+        except:
+            # If still no answer
+            print_say('I am still learning. I don\'t know the answer to your question yet.')
+            
+        
