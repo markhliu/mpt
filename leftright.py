3541c1ac7adbc183c9b9c9d407f046a360d4a977
Noah Spahn <noah.de@gmail.com>
2020-08-23 12:47:17 -0700

    testing file for a demo

---
+++ leftright.py
@@ -0,0 +1,29 @@
+import turtle as t
+
+t.Screen()
+t.setup(600,500,100,200)
+t.bgcolor('light blue')
+t.title('Python Turtle Graphics')
+t.pensize(5)
+t.right(30)
+t.forward(200)
+t.left(30)
+t.backward(400)
+t.left(90)
+t.pencolor('red')
+t.forward(200)
+t.pencolor('teal')
+t.right(90)
+t.forward(200)
+t.pencolor('yellow')
+t.right(90)
+t.forward(100)
+t.pencolor('orange')
+t.left(90)
+t.forward(30)
+t.done()
+try:
+    t.bye()
+except t.Terminator:
+    pass
+
