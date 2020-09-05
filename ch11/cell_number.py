76faacf6b150104291ed98db03d045cedf4f20be
markhliu <50116107+markhliu@users.noreply.github.com>
2020-09-05 13:45:42 -0400

    Update cell_number.py

--- ch11/cell_number.py
+++ ch11/cell_number.py
@@ -1,31 +1,30 @@
-from turtle import *
+import turtle as t
 
 # Set up the screen
-Screen()
-setup(600,600, 10, 70)
-tracer(False)
-bgcolor("red")
-title("Tic-Tac-Toe in Turtle Graphics")
+t.setup(600,600, 10, 70)
+t.tracer(False)
+t.bgcolor("red")
+t.title("Tic-Tac-Toe in Turtle Graphics")
 # Draw horizontal lines and vertical lines to form grid
-pensize(5)
+t.pensize(5)
 for i in (-100,100):  
-    up()
-    goto(i, -300)
-    down()
-    goto(i, 300)
-    up()
-    goto(-300,i)
-    down()
-    goto(300,i)
-    up()
+    t.up()
+    t.goto(i, -300)
+    t.down()
+    t.goto(i, 300)
+    t.up()
+    t.goto(-300,i)
+    t.down()
+    t.goto(300,i)
+    t.up()
 # Create a dictionary to map cell number to the cell center coordinates
 cellcenter={'1':(-200,-200), '2':(0,-200), '3':(200,-200),
             '4':(-200,0), '5':(0,0), '6':(200,0),
             '7':(-200,200), '8':(0,200), '9':(200,200)} 
 # Go to the center of each cell, write down the cell number
 for cell, center in list(cellcenter.items()):
-    goto(center)
-    write(cell,font=('Arial',20,'normal'))
+    t.goto(center)
+    t.write(cell,font=('Arial',20,'normal'))
 # Define a function CellNumber() to print out the cell number
 def CellNumber(x,y):
     if -300<x<300 and -300<y<300:
@@ -41,12 +40,12 @@ def CellNumber(x,y):
     else:
         print('you have clicked outside the game board')
 # Hide turtle so that you don't see the arrowhead        
-hideturtle()
+t.hideturtle()
 # Bind the mouse click to the CellNumber() function
-onscreenclick(CellNumber)
-listen()    
-done()
+t.onscreenclick(CellNumber)
+t.listen()    
+t.done()
 try:
-    bye()
-except Terminator:
-    pass
+    t.bye()
+except t.Terminator:
+    print('exit turtle')
