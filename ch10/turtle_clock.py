477c3721c5e4c19c2df41e63961ffa22ec87d442
markhliu <50116107+markhliu@users.noreply.github.com>
2020-09-07 08:55:12 -0400

    Update turtle_clock.py

--- ch10/turtle_clock.py
+++ ch10/turtle_clock.py
@@ -1,33 +1,34 @@
-from turtle import *
+import turtle as t
 import time
+
 import arrow
 
-# Set up the screen    
-setup(800,600, 10, 70)
-tracer(False)
-bgcolor('lightgreen')
-hideturtle()
+# Set up the screent.
+t.setup(800,600, 10, 70)
+t.tracer(False)
+t.bgcolor('lightgreen')
+t.hideturtle()
 # Put the program in an infinite loop
 while True:
     # Clear the screen
-    clear()
+    t.clear()
     # Obtain the current time
-    current_time = arrow.utcnow()
-    local = current_time.to('US/Pacific').format('hh:mm:ss A')
-    color('blue')
-    up()
-    goto(-300,50)
+    current_time = arrow.utcnow().format('hh:mm:ss A')
+    t.color('blue')
+    t.up()
+    t.goto(-300,50)
     # Write the first line of text
-    write('The Current Time Is\n',font=('Arial',50,'normal'))
-    color('red')
-    goto(-300,-100)
+    t.write('The Current Time Is\n',font=('Arial',50,'normal'))
+    t.color('red')
+    t.goto(-300,-100)
     # Write what time it is
-    write(local,font=('Arial',80,'normal'))
+    t.write(current_time,font=('Arial',80,'normal'))
     time.sleep(1)
     # Put everything on screen
-    update()
-done()
+    t.update()
+t.done()
 try:
-    bye()
-except Terminator:
-    pass
+    t.bye()
+except t.Terminator:
+    print('exit turtle')
+
