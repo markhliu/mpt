da83df39e2769af7e2b39ac4ef67fb31f4aeb376
markhliu <50116107+markhliu@users.noreply.github.com>
2020-09-13 10:26:36 -0400

    Update cell_number.py

--- ch11/cell_number.py
+++ ch11/cell_number.py
@@ -18,13 +18,13 @@ for i in (-100,100):
     t.goto(300,i)
     t.up()
 # Create a dictionary to map cell number to the cell center coordinates
-cellcenter={'1':(-200,-200), '2':(0,-200), '3':(200,-200),
+cellcenter = {'1':(-200,-200), '2':(0,-200), '3':(200,-200),
             '4':(-200,0), '5':(0,0), '6':(200,0),
             '7':(-200,200), '8':(0,200), '9':(200,200)} 
 # Go to the center of each cell, write down the cell number
 for cell, center in list(cellcenter.items()):
     t.goto(center)
-    t.write(cell,font=('Arial',20,'normal'))
+    t.write(cell,font = ('Arial',20,'normal'))
 # Define a function cell_number() to print out the cell number
 def cell_number(x,y):
     if -300<x<300 and -300<y<300:
