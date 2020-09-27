eb24568294af39d80d4675f49dfd6c6fc6145974
markhliu <50116107+markhliu@users.noreply.github.com>
2020-09-27 03:53:27 -0400

    Update show_disc.py

--- ch12/show_disc.py
+++ ch12/show_disc.py
@@ -24,19 +24,19 @@ for i in range(-200,300,100):
     t.goto(350,i)
     t.up()
 # Write column numbers on the board
-colnum=1
+colnum = 1
 for x in range(-300, 350, 100):
     t.goto(x,270)
-    t.write(colnum,font=('Arial',20,'normal'))
+    t.write(colnum,font = ('Arial',20,'normal'))
     colnum += 1
 # The red player moves first
-turn="red"
-# The x-coordinates of the center of the 7 columns
-xs=[-300,-200,-100,0,100,200,300]
-# The y-coordinates of the center of the 6 rows
-ys=[-250,-150,-50,50,150,250]
+turn = "red"
+# The x coordinates of the center of the 7 columns
+xs = [-300,-200,-100,0,100,200,300]
+# The y coordinates of the center of the 6 rows
+ys = [-250,-150,-50,50,150,250]
 # Keep track of the occupied cells
-occupied=[list(),list(),list(),list(),list(),list(),list()]
+occupied = [list(),list(),list(),list(),list(),list(),list()]
 # Define a function conn() to place a disc in a cell
 def conn(x,y):
     # Make the variable turn a globale variable
@@ -47,7 +47,7 @@ def conn(x,y):
     else:
         print('You have clicked outside the game board!')
     # Calculate the lowest available row number in that column
-    row=len(occupied[col-1])+1
+    row = len(occupied[col-1])+1
     # Go to the cell and place a dot of the player's color
     t.up()
     t.goto(xs[col-1],ys[row-1])
@@ -55,10 +55,10 @@ def conn(x,y):
     # Add the move to the occupied list to keep track
     occupied[col-1].append(turn)
     # Give the turn to the other player
-    if turn=="red":
-        turn="yellow"
+    if turn == "red":
+        turn = "yellow"
     else:
-        turn="red"     
+        turn = "red"     
 # Bind the mouse click to the conn() function
 t.onscreenclick(conn)
 t.listen()    
@@ -67,3 +67,4 @@ try:
     t.bye()
 except t.Terminator:
     print('exit turtle')
+
