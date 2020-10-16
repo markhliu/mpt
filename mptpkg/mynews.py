5fe9a12a503d9babd483ed2525a7e6f4b73976b0
markhliu <50116107+markhliu@users.noreply.github.com>
2020-10-16 10:44:24 -0400

    Update mynews.py

--- mptpkg/mynews.py
+++ mptpkg/mynews.py
@@ -9,17 +9,17 @@ def news_brief():
     # Locate the website for the NPR news brief
     url = 'https://www.npr.org/podcasts/500005/npr-news-now'
     # Convert the source code to a soup string
-    response=requests.get(url)
+    response = requests.get(url)
     response.raise_for_status()
     soup = bs4.BeautifulSoup(response.text, 'html.parser')   
     # Locate the tag that contains the mp3 files
     casts = soup.findAll('a', {'class': 'audio-module-listen'})
     # Obtain the weblink for the mp3 file related to the latest news brief
-    cast=casts[0]['href']
-    pos=cast.find("?")
+    cast = casts[0]['href']
+    pos = cast.find("?")
     # Download the mp3 file
-    mymp3=cast[0:pos]
-    x=choice(range(1000000))
+    mymp3 = cast[0:pos]
+    x = choice(range(1000000))
     mymp3 = requests.get(mymp3)
     with open(f'f{x}.mp3','wb') as f:
         f.write(mymp3.content)
