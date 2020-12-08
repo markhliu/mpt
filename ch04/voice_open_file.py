e0b6531b342cc2d901781a59a96da68260ca37a2
Mark Liu <50116107+markhliu@users.noreply.github.com>
2020-12-08 09:22:11 -0500

    Update voice_open_file.py

--- ch04/voice_open_file.py
+++ ch04/voice_open_file.py
@@ -5,6 +5,8 @@ import platform
 import speech_recognition as sr 
 
 speech = sr.Recognizer()
+directory = pathlib.Path.cwd()
+
 def voice_to_text():
     voice_input = "" 
     with sr.Microphone() as source:
@@ -21,40 +23,40 @@ def voice_to_text():
     return voice_input
 def open_file(filename):
     if platform.system() == "Windows":
-        os.system(f"explorer {filename}") 
+        os.system(f"explorer {directory}\\files\\{filename}") 
     elif platform.system() == "Darwin":
-        os.system(f"open {filename}") 
+        os.system(f"open {directory}/files/{filename}") 
     else:
-        os.system(f"xdg-open {filename}") 
+        os.system(f"xdg-open {directory}/files/{filename}")
 while True:   
     print('Python is listening...')
-    inp=voice_to_text().lower()
+    inp = voice_to_text().lower()
     print(f'You just said {inp}.')
-    if inp=="stop listening":
+    if inp == "stop listening":
         print('Goodbye!')
         break
     elif "open pdf" in inp: 
-        inp=inp.replace('open pdf ','')
-        myfile = pathlib.Path.cwd()/'files'/f'{inp}.pdf)'
+        inp = inp.replace('open pdf ','')
+        myfile = f'{inp}.pdf'
         open_file(myfile)
         continue
     elif "open word" in inp: 
-        inp=inp.replace('open word ','')
-        myfile = pathlib.Path.cwd()/'files'/f'{inp}.docx)'
+        inp = inp.replace('open word ','')
+        myfile = f'{inp}.docx'
         open_file(myfile)
         continue
     elif "open excel" in inp: 
-        inp=inp.replace('open excel ','')
-        myfile = pathlib.Path.cwd()/'files'/f'{inp}.xlsx)'
+        inp = inp.replace('open excel ','')
+        myfile = f'{inp}.xlsx'
         open_file(myfile)
         continue
     elif "open powerpoint" in inp: 
-        inp=inp.replace('open powerpoint ','')
-        myfile = pathlib.Path.cwd()/'files'/f'{inp}.pptx)'
+        inp = inp.replace('open powerpoint ','')
+        myfile = f'{inp}.pptx'
         open_file(myfile)
         continue
     elif "open audio" in inp: 
-        inp=inp.replace('open audio ','')
-        myfile = pathlib.Path.cwd()/'files'/f'{inp}.mp3)'
+        inp = inp.replace('open audio ','')
+        myfile = f'{inp}.mp3'
         open_file(myfile)
         continue
