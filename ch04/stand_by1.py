ccdf09058dde7740b6911cf182dc817c70cf8e18
Mark Liu <50116107+markhliu@users.noreply.github.com>
2020-12-08 09:17:53 -0500

    Update stand_by1.py

--- ch04/stand_by1.py
+++ ch04/stand_by1.py
@@ -3,8 +3,8 @@ from mysr import voice_to_text
 
 while True:   
     print('Python is listening...')
-    inp= voice_to_text()
+    inp = voice_to_text()
     print(f'You just said {inp}.')
-    if inp=="stop listening":
+    if inp == "stop listening":
         print('Goodbye!')
         break
