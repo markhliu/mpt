353ef3c54c06f8cc4de4971dc8838b3fdb25a94f
Mark Liu <50116107+markhliu@users.noreply.github.com>
2020-12-09 09:42:53 -0500

    Update wiki_world_languages.py

--- ch17/wiki_world_languages.py
+++ ch17/wiki_world_languages.py
@@ -6,44 +6,48 @@ from pydub import AudioSegment
 from pydub.playback import play
 import wikipedia
 
+from mptpkg import print_say
+
 # Create a dictionary of languages and the corresponding codes
-lang_abbre={"english":"en",
+lang_abbre = {"english":"en",
             "chinese":"zh-cn",
             "spanish":"es-es",
             "french":"fr-fr",
             "japanese":"ja",
-            "portuguesE":"pt",
+            "portuguese":"pt",
             "russian":"ru",
             "korean":"ko",
             "german":"de",
             "italian":"it"}
-lang=input("What language you want to use?\n")
 
-# Initiate speech recognition
-speech = sr.Recognizer()      
+lang = input("What language do you want to use?\n")
+    
 # Ask what you want to know
-print(f"Say what you want to know in {lang}...")
+print_say(f"Say what you want to know in {lang}...")
+# Initiate speech recognition
+speech = sr.Recognizer()  
 # Capture your voice query in the language of your choice
 with sr.Microphone() as source:
     speech.adjust_for_ambient_noise(source)
-    try:
-        audio = speech.listen(source)
-        my_input = speech.recognize_google(audio, language=lang_abbre[lang][0:2])   
-    except sr.UnknownValueError:
-        pass
+    while True:
+        try:
+            audio = speech.listen(source)
+            my_input = speech.recognize_google(audio, language=lang_abbre[lang][0:2])
+            break
+        except sr.UnknownValueError:
+            print_say("Sorry, I cannot understand what you said!")
 # Print out what you said
 print(f"you said: {my_input}")
 # Obtain answer from Wikipedia and print out
 wikipedia.set_lang(lang_abbre[lang][0:2])
-ans=wikipedia.summary(my_input)[0:200]
+ans = wikipedia.summary(my_input)[0:200]
 print(ans)
 # Convert text to speech in the language of your choice
-tts=gTTS(text=ans,lang=lang_abbre[lang])
+tts  =gTTS(text=ans,lang=lang_abbre[lang])
 # Create a temporary file 
-voice=BytesIO()
+voice = BytesIO()
 # Save the voice output as an audio file
 tts.write_to_fp(voice)
 # Play the audio file
 voice.seek(0)
 play(AudioSegment.from_mp3(voice))
-
