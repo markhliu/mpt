e8c76fa75612db7f5ebef1fcce4b59ba42a140d7
Mark Liu <50116107+markhliu@users.noreply.github.com>
2020-12-09 09:40:14 -0500

    Update speak_world_languages.py

--- ch17/speak_world_languages.py
+++ ch17/speak_world_languages.py
@@ -5,22 +5,22 @@ from pydub import AudioSegment
 from pydub.playback import play
 
 # Create a dictionary of languages and the corresponding codes
-lang_abbre={"english":"en",
+lang_abbre = {"english":"en",
             "chinese":"zh-cn",
             "spanish":"es-es",
             "french":"fr-fr",
             "japanese":"ja",
-            "portuguesE":"pt",
+            "portuguese":"pt",
             "russian":"ru",
             "korean":"ko",
             "german":"de",
             "italian":"it"}
-lang=input("What language you want to use?\n")
-phrase=input("What phrase you want to convert to voice?\n")
+lang = input("What language you want to use?\n")
+phrase = input("What phrase you want to convert to voice?\n")
 # Convert text to speech
-tts=gTTS(text=phrase,lang=lang_abbre[lang])
+tts = gTTS(text=phrase,lang=lang_abbre[lang])
 # Create a temporary file 
-voice=BytesIO()
+voice = BytesIO()
 # Save the voice output as an audio file
 tts.write_to_fp(voice)
 # Play the audio file
