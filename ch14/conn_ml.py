7abcb767e6e5f4c080c6e7010a746c92c5f825b4
Mark Liu <50116107+markhliu@users.noreply.github.com>
2021-03-01 11:00:36 -0500

    Update conn_ml.py

--- ch14/conn_ml.py
+++ ch14/conn_ml.py
@@ -52,7 +52,11 @@ def horizontal4(x, y, color, board):
     win = False
     for dif in (-3, -2, -1, 0):
         try:
-            if board[x+dif][y] == color and board[x+dif+1][y] == color and board[x+dif+2][y] == color and board[x+dif+3][y] == color and  x+dif >=  0:
+            if board[x+dif][y] == color\
+            and board[x+dif+1][y] == color\
+            and board[x+dif+2][y] == color\
+            and board[x+dif+3][y] == color\
+            and  x+dif >=  0:
                 win = True            
         except IndexError:
             pass
@@ -61,7 +65,11 @@ def horizontal4(x, y, color, board):
 def vertical4(x, y, color, board):
     win = False
     try:
-        if board[x][y] == color and board[x][y-1] == color and board[x][y-2] == color and board[x][y-3] == color and y-3 >=  0:
+        if board[x][y] == color\
+        and board[x][y-1] == color\
+        and board[x][y-2] == color\
+        and board[x][y-3] == color\
+        and y-3 >=  0:
             win = True     
     except IndexError:
         pass
@@ -71,7 +79,11 @@ def forward4(x, y, color, board):
     win = False
     for dif in (-3, -2, -1, 0):
         try:
-            if board[x+dif][y+dif] == color and board[x+dif+1][y+dif+1] == color and board[x+dif+2][y+dif+2] == color and board[x+dif+3][y+dif+3] == color and x+dif >=  0 and y+dif >=  0:
+            if board[x+dif][y+dif] == color\
+            and board[x+dif+1][y+dif+1] == color\
+            and board[x+dif+2][y+dif+2] == color\
+            and board[x+dif+3][y+dif+3] == color\
+            and x+dif >=  0 and y+dif >=  0:
                 win = True            
         except IndexError:
             pass
@@ -81,7 +93,11 @@ def back4(x, y, color, board):
     win = False
     for dif in (-3, -2, -1, 0):
         try:
-            if board[x+dif][y-dif] == color and board[x+dif+1][y-dif-1] == color and board[x+dif+2][y-dif-2] == color and board[x+dif+3][y-dif-3] == color and x+dif >=  0 and y-dif-3 >=  0:
+            if board[x+dif][y-dif] == color\
+            and board[x+dif+1][y-dif-1] == color\
+            and board[x+dif+2][y-dif-2] == color\
+            and board[x+dif+3][y-dif-3] == color\
+            and x+dif >=  0 and y-dif-3 >=  0:
                 win = True            
         except IndexError:
             pass
@@ -135,7 +151,7 @@ def best_move():
     for move in validinputs:
         if len(outcomes[move])>0:
             outcome = sum(outcomes[move])/len(outcomes[move])
-            # If the average outcome from that move beats the current best move
+            # If the average outcome beats the current best
             if outcome>bestoutcome:
                 # Update the bestoutcome
                 bestoutcome = outcome
@@ -248,4 +264,3 @@ try:
     t.bye()
 except t.Terminator:
     print('exit turtle')
-
