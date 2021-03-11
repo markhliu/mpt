b59326c64f37878f53b609e12869cf944e7cb17f
Mark Liu <50116107+markhliu@users.noreply.github.com>
2021-03-11 18:27:19 -0500

    Add files via upload

---
+++ ch01/my_first_script.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jan  6 06:28:19 2021
+
+@author: hlliu2
+"""
+
+print("This is my very first Python script!")
+
+print("This is my second Python message!")
