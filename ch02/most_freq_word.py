fea886d5abed631514790a0b7bd75b7b601c393e
Mark Liu <50116107+markhliu@users.noreply.github.com>
2021-03-12 07:18:02 -0500

    Update most_freq_word.py

--- ch02/most_freq_word.py
+++ ch02/most_freq_word.py
@@ -1,18 +1,19 @@
-news=(
-'''Python is an interpreted, high-level and general-purpose programming
- language. Python's design philosophy emphasizes code readability with
- its notable use of significant whitespace. 
- Its language constructs and object-oriented approach aim to help 
- programmers write clear, logical code for small and large-scale 
- projects.
-''')
-wdcnt=dict()
-wd=news.split()
+news = (
+    '''Python is an interpreted, high-level and general-purpose programming
+     language. Python's design philosophy emphasizes code readability with
+     its notable use of significant whitespace. 
+     Its language constructs and object-oriented approach aim to help 
+     programmers write clear, logical code for small and large-scale 
+     projects.
+    ''')
+wdcnt = dict()
+wd = news.split()
 for w in wd:
-    wdcnt[w]=wdcnt.get(w,0)+1
-print(wdcnt)   
+    wdcnt[w] = wdcnt.get(w, 0) + 1
+print(wdcnt)
 for w in list(wdcnt.keys()):
-    if wdcnt[w]==max(list(wdcnt.values())):
+    if wdcnt[w] == max(list(wdcnt.values())):
         print(w)
+
 
 
