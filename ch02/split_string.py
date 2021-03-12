69d14e879320c8ce4cebcfc968860e4f6873200a
Mark Liu <50116107+markhliu@users.noreply.github.com>
2021-03-12 07:13:08 -0500

    Update split_string.py

--- ch02/split_string.py
+++ ch02/split_string.py
@@ -1,6 +1,6 @@
-email= "John.Smith@uky.edu"
-(name,domain)=email.split('@')
-(first,last)=name.split('.')
+email = "John.Smith@uky.edu"
+(name, domain) = email.split('@')
+(first, last) = name.split('.')
 print(f"last name is {last}")
 
 
