1ea486c163ca8159f3b8c5d3d0a6b7cb99d878e3
Mark Liu <50116107+markhliu@users.noreply.github.com>
2021-03-12 07:11:57 -0500

    Update extract_last_name.py

--- ch02/extract_last_name.py
+++ ch02/extract_last_name.py
@@ -1,7 +1,8 @@
-email= "John.Smith@uky.edu"
-pos1=email.find(".")
+email = "John.Smith@uky.edu"
+pos1 = email.find(".")
 print(pos1)
-pos2=email.find("@")
+pos2 = email.find("@")
 print(pos2)
-last_name=email[(1+pos1):pos2]
+last_name = email[(1 + pos1):pos2]
 print(last_name)
+
