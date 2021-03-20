6fc8001daddb25332a895bb7b4d8e500c96188ed
Mark Liu <50116107+markhliu@users.noreply.github.com>
2021-03-20 09:45:36 -0400

    Update test_pydub.py

--- appendix/test_pydub.py
+++ appendix/test_pydub.py
@@ -1,5 +1,6 @@
+# Make sure you pat hello.mp3 in the same folder as this script
 from pydub import AudioSegment
 from pydub.playback import play
 
-play(AudioSegment.from_mp3("../hello.mp3"))
+play(AudioSegment.from_mp3("hello.mp3"))
 
