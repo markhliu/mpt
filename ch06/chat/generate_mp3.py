e45873429d7bc177201e80ecf7dba81bd5e52c57
Mark Liu <50116107+markhliu@users.noreply.github.com>
2021-03-28 04:29:50 -0400

    Add files via upload

---
+++ ch06/chat/generate_mp3.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Mar 28 04:15:00 2021
+
+@author: OlddeskSSD
+"""
+
+from gtts import gTTS
+tts = gTTS('replace this file with a song by Katy Perry', lang='en')
+tts.save('KatyPerry.mp3')
