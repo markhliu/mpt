920fc6b8769df64cc1fc5189d39506ed0fc3ed34
Mark Liu <50116107+markhliu@users.noreply.github.com>
2021-03-28 07:39:44 -0400

    Update __init__.py

--- mptpkg/__init__.py
+++ mptpkg/__init__.py
@@ -1,12 +1,20 @@
 
 from .mysr import voice_to_text
 from .mysay import print_say
+
+''' Starting in Chapter 7, add the following five lines in this file 
 from .mywakeup import wakeup
 from .mytimer import timer
 from .myalarm import alarm
 from .myjoke import joke
 from .myemail import email
+'''
+
+''' Starting in Chapter 8, add the following line in this file 
 from .myknowall import know_all
+'''
+
+''' In Chapter 17, add the following in this file
 from .mymusic import music_play, music_stop
 from .mynews import news_brief, news_stop
 from .myradio import live_radio, radio_stop
@@ -14,3 +22,4 @@ from .myttt import ttt
 from .myconn import conn
 from .mystock import stock_market, stock_price
 from .mytranslate import voice_translate
+'''
