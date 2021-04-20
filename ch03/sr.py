bcd44404621d03ecd69bfbca0aae6d7af21e82ed
Mark Liu <50116107+markhliu@users.noreply.github.com>
2021-04-20 04:24:29 -0400

    Update sr.py

--- ch03/sr.py
+++ ch03/sr.py
@@ -1,7 +1,9 @@
 import speech_recognition as sr
+
 speech = sr.Recognizer()
 print('Python is listening...')
 with sr.Microphone() as source:
+    speech.adjust_for_ambient_noise(source)
     audio = speech.listen(source)
     inp = speech.recognize_google(audio)
 print(f'You just said {inp}.') 
