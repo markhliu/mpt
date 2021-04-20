2b8dd195b8b042a027f23772f1141d309ac9ff2c
Mark Liu <50116107+markhliu@users.noreply.github.com>
2021-04-20 03:58:37 -0400

    Update most_freq_word.py

--- ch02/most_freq_word.py
+++ ch02/most_freq_word.py
@@ -3,7 +3,7 @@ news = (
      language. Python's design philosophy emphasizes code readability with
      its notable use of significant whitespace. 
      Its language constructs and object-oriented approach aim to help 
-     programmers write clear, logical code for small and large-scale 
+     programmers write clear, logical code for small- and large-scale 
      projects.
     ''')
 wdcnt = dict()
