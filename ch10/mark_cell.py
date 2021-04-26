7f9425f2470cf776e2bbcccc120fe23ac5eca20a
Mark Liu <50116107+markhliu@users.noreply.github.com>
2021-04-26 09:29:19 -0400

    Update mark_cell.py

--- ch10/mark_cell.py
+++ ch10/mark_cell.py
@@ -1,7 +1,7 @@
 import turtle as t
 
 # Set up the screen
-t.setup(600,600, 10, 70)
+t.setup(600,600,10,70)
 t.tracer(False)
 t.bgcolor("red")
 t.title("Tic-Tac-Toe in Turtle Graphics")
@@ -9,15 +9,15 @@ t.title("Tic-Tac-Toe in Turtle Graphics")
 t.pensize(5)
 for i in (-100,100):  
     t.up()
-    t.goto(i, -300)
+    t.goto(i,-300)
     t.down()
-    t.goto(i, 300)
+    t.goto(i,300)
     t.up()
     t.goto(-300,i)
     t.down()
     t.goto(300,i)
     t.up()
-# Create a dictionary to map cell number to the cell center coordinates
+# Create a dictionary to map cell numbers to the center coordinates
 cellcenter = {'1':(-200,-200), '2':(0,-200), '3':(200,-200),
             '4':(-200,0), '5':(0,0), '6':(200,0),
             '7':(-200,200), '8':(0,200), '9':(200,200)} 
@@ -36,7 +36,7 @@ def mark_cell(x,y):
         col = int((x+500)//200)
         row = int((y+500)//200)
         # The cell number is a string varibale
-        cellnumber = str(col + (row - 1)*3)
+        cellnumber = str(col+(row-1)*3)
     else:
         print('you have clicked outside the game board')
     # Go to the corresponding cell and place a dot of the player's color
