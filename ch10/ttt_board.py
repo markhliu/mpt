a4698396f0246dd0370d0e6a1042e289b3bf91d4
Mark Liu <50116107+markhliu@users.noreply.github.com>
2021-04-26 09:30:13 -0400

    Update ttt_board.py

--- ch10/ttt_board.py
+++ ch10/ttt_board.py
@@ -1,7 +1,7 @@
 import turtle as t
 
 # Set up the screen
-t.setup(600,600, 10, 70)
+t.setup(600,600,10,70)
 t.tracer(False)
 t.bgcolor("red")
 t.hideturtle()
@@ -10,15 +10,15 @@ t.title("Tic-Tac-Toe in Turtle Graphics")
 t.pensize(5)
 for i in (-100,100):  
     t.up()
-    t.goto(i, -300)
+    t.goto(i,-300)
     t.down()
-    t.goto(i, 300)
+    t.goto(i,300)
     t.up()
     t.goto(-300,i)
     t.down()
     t.goto(300,i)
     t.up()
-# Create a dictionary to map cell number to the cell center coordinates
+# Create a dictionary to map cell numbers to cell center coordinates
 cellcenter = {'1':(-200,-200), '2':(0,-200), '3':(200,-200),
             '4':(-200,0), '5':(0,0), '6':(200,0),
             '7':(-200,200), '8':(0,200), '9':(200,200)} 
