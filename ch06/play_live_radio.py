8c26e2f354c34be8ef3498f11529295e48f00264
Mark Liu <50116107+markhliu@users.noreply.github.com>
2021-08-11 18:32:24 -0400

    Update play_live_radio.py

--- ch06/play_live_radio.py
+++ ch06/play_live_radio.py
@@ -1,5 +1,5 @@
 # Put your web driver in the same folder as this script
-from selenium import web driver
+from selenium import webdriver
 browser = webdriver.Chrome(executable_path='./chromedriver')
 browser.get("https://onlineradiobox.com/us/")
 button = browser.find_element_by_xpath('//*[@id="b_top_play"]')
